a30ca10b7031ada717b39b0e145bb99b3eefb7fa
pedramkarimii <pedram.9060@gmail.com>
2024-03-01 20:12:32 +0330

    :triangular_flag_on_post: profile change inherit models.Model add __str__

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from core.mixin import BaseModelUserMixin
 from django.urls import reverse
 
+
 class User(BaseModelUserMixin):
     """
         Model for users.
@@ -12,7 +13,6 @@ class User(BaseModelUserMixin):
         Attributes:
             email (EmailField): The email address of the user.
             phone_number (CharField): The phone number of the user.
-            full_name (CharField): The full name of the user.
             create_time (DateTimeField): The timestamp indicating the creation time of the user record.
             update_time (DateTimeField): The timestamp indicating the last update time of the user record.
             is_deleted (BooleanField): Flag indicating whether the user has been deleted or not.
@@ -29,20 +29,20 @@ class User(BaseModelUserMixin):
             has_module_perms: Method to check if the user has permissions for a specific module.
             capitalize: Property method that returns the full name of the user with each word capitalized.
         """
-    pass
+
+    def __str__(self):
+        return f"ID: {self.id}"
 
 
-class Profile(BaseModelUserMixin):
+class Profile(models.Model):
     """
         Model for user profiles.
 
         This class represents a user profile model, containing additional information about the user
-        such as their image and associated basket. It inherits common user-related fields and methods
-        from BaseModelUserMixin.
+        It inherits common user-related fields and methods from models.Model.
 
         Attributes:
             image (ImageField): Field to store the user's profile image.
-            basket (OneToOneField): Relationship to the user's basket using a OneToOneField.
             user (OneToOneField): Relationship to the user model using a OneToOneField.
             email (EmailField): The email address of the user.
             phone_number (CharField): The phone number of the user.
@@ -50,13 +50,6 @@ class Profile(BaseModelUserMixin):
             create_time (DateTimeField): The timestamp indicating the creation time of the user record.
             update_time (DateTimeField): The timestamp indicating the last update time of the user record.
             is_deleted (BooleanField): Flag indicating whether the user has been deleted or not.
-            is_active (BooleanField): Flag indicating whether the user is active or not.
-            is_admin (BooleanField): Flag indicating whether the user has admin privileges.
-            is_staff (BooleanField): Flag indicating whether the user is staff or not.
-            is_superuser (BooleanField): Flag indicating whether the user is a superuser or not.
-            USERNAME_FIELD (str): Field used for authentication, in this case, the phone number.
-            REQUIRED_FIELDS (list): List of fields required for creating a user, in this case, email and full name.
-            objects (UserManager): Custom manager for handling user queries.
 
         Meta:
             ordering (list): Specifies the default ordering of profile records, in this case, by username.
@@ -65,13 +58,30 @@ class Profile(BaseModelUserMixin):
 
         Methods:
             get_absolute_url: Method to return the absolute URL of the profile instance.
-            has_perm: Method to check if the user has a specific permission.
-            has_module_perms: Method to check if the user has permissions for a specific module.
         """
-    image = models.ImageField(upload_to='images/profile/%Y/%m/%d/')
-    basket = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='profile')
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    email = models.EmailField(max_length=200, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/profile/%Y/%m/%d/')
+    create_time = models.DateTimeField(auto_now_add=True, editable=False)
+    update_time = models.DateTimeField(auto_now=True, editable=False)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-update_time', '-is_active']
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        return f"ID: {self.id} full name: {self.full_name}"
 
     def get_absolute_url(self):
         """Method to return the absolute URL of the profile instance."""
         return reverse("profile_detail", args=[self.id])
+
+    @property
+    def capitalize(self):
+        """Property method that returns the full name of the user with each word capitalized."""
+        return self.full_name.title()
