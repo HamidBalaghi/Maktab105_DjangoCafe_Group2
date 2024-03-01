079aef39ea2b07356dbf042db0d4a32e1e440047
pedramkarimii <pedram.9060@gmail.com>
2024-03-01 20:32:39 +0330

    :triangular_flag_on_post: creat: create_user, create_admin, create_superuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,5 +1,35 @@
 from django.contrib.auth.models import BaseUserManager
 
 
-class UserManager(BaseUserManager):  # TODO: add custom user manager
-    pass
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, email, password):
+        if not phone_number:
+            raise ValueError('The phone number must be set')
+        elif not email:
+            raise ValueError('The Email must be set')
+        user = self.model(phone_number=phone_number, email=self.normalize_email(email))
+        user.is_admin = False
+        user.is_staff = False
+        user.is_superuser = False
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_admin(self, phone_number, email, password):
+        user = self.create_user(phone_number, email, password)
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = False
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, email, password):
+        user = self.create_user(phone_number, email, password)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.set_password(password)
+        user.save()
+        return user
+
