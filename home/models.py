bbed3ccf7d2d95983000c86d3825cd9ab3b410f0
pedramkarimii <pedram.9060@gmail.com>
2024-03-01 20:13:55 +0330

    add TODO

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class Category(models.Model):  # TODO:delete this app
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='category_images/')
     sub_of = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
--- product/models.py
+++ product/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
         return self.name
 
 
-class OrderProduct(models.Model):
+class OrderProduct(models.Model):  # TODO: Creat app and move
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     count = models.IntegerField(default=1)
