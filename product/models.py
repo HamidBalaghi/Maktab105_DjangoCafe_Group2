9c66bef36a50324ed48fe89db5cb48f10defb679
ashkan1592 <ashkan1592@gmail.com>
2024-03-01 08:14:38 +0330

    add OrderProduct model

--- product/models.py
+++ product/models.py
@@ -13,7 +13,16 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category, related_name='products')
+    categories = models.ManyToManyField(Category, related_name="products")
 
     def __str__(self):
         return self.name
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ("order", "product")
