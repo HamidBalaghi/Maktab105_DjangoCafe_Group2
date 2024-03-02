7b3b8dde38222bafaaa4fd7f8f27a0f7ef7d471a
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-02 21:39:24 +0330

    Integration of product and order models

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
+from product.models import Product, Table
 
 
-class OrderProduct(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    count = models.IntegerField(default=1)
+# Create your models here.
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
         unique_together = ("order", "product")
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    paid_time = models.DateTimeField(null=True, blank=True, auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id} | User: {self.user}"
+
+
+class ReserveTable(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    is_reserved = models.BooleanField(default=False)
+    reserved_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    repay_time = models.DateTimeField(null=True, blank=True, auto_now=True)
+
--- product/models.py
+++ product/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='category_images/')
-    sub_of = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Product(models.Model):
@@ -18,9 +11,21 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category, related_name="products")
+    categories = models.ManyToManyField('Category', related_name="products")
 
     def __str__(self):
         return self.name
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category_images/')
+    sub_of = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+    id = models.AutoField(primary_key=True)
 
