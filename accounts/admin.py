65d8e9ff9f5ccc189cd8bf99e8db6477127f5f66
pedramkarimii <pedram.9060@gmail.com>
2024-03-03 19:23:35 +0330

    creat profile user inline

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,32 +1,25 @@
 from django.contrib import admin
-from accounts.models import User, Profile
-# from .forms import UserCreationForm, UserChangeForm
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, Group
-#
-#
-# class UserAdmin(BaseUserAdmin):
-#     form = UserChangeForm
-#     add_form = UserCreationForm
-#     model = User
-#     list_display = ('email', 'phone_number', 'is_admin', 'is_superuser', 'is_staff', 'is_active')
-#     list_filter = ('is_admin', 'is_active')
-#     fieldsets = (
-#         ('Change personal info', {'fields': ('email', 'phone_number', 'password')}),
-#         ('Permissions',
-#          {'fields': ('is_superuser', 'is_active', 'is_admin', 'is_staff', 'is_deleted', 'create_time', 'update_time',
-#                      'last_login', 'user_groups', 'user_permissions_set')}),
-#     )
-#
-#     add_fieldsets = (
-#         ('Creation User', {
-#             'fields': ('email', 'phone_number', 'password1', 'password2')}
-#          ),
-#     )
-#     search_fields = ('email',)
-#     ordering = ('email',)
-#     filter_horizontal = ('user_groups', 'user_permissions_set')
-#
-#
-admin.site.register((Profile, User))
-# admin.site.register(User, UserAdmin)
-# admin.site.unregister(Group)
+from accounts.models import  Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class ExtendedUserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(ExtendedUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
+admin.site.register(Profile)
