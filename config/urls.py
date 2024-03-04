8015092ecf75a6699e90f84c7fd5f4e2044d2ce4
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-04 22:17:17 +0330

    Add cart URL

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('menu/', include('menu.urls')),
+    path('cart/', include('orders.urls')),
 ]
