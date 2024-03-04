398b47974ab8d4977908a267bf3cc2f644073ea1
Amirali Qobadi <amiraliqobadi5@gmail.com>
2024-03-04 17:00:41 +0330

    i changed home url to " "

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, EditeUserView, RegisterView, CreateUserView, ChangePasswordView, \
-    CreateProfileView, HomeView, ProfileDetailView
+from .views import (
+    UserLoginView,
+    UserLogoutView,
+    EditeUserView,
+    RegisterView,
+    CreateUserView,
+    ChangePasswordView,
+    CreateProfileView,
+    HomeView,
+    ProfileDetailView,
+)
+
 """ 
 URL pattern for the home page
 URL pattern for user login
@@ -13,15 +23,13 @@ URL pattern for changing password
 URL pattern for Info profile 
 """
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('creatuser/', CreateUserView.as_view(), name='creat_user'),
-    path('creatprofile/', CreateProfileView.as_view(), name='creat_profile'),
-    path('editeuser/', EditeUserView.as_view(), name='edit_user'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('changepass/', ChangePasswordView.as_view(), name='change_pass'),
-    path('profiles/<int:pk>/', ProfileDetailView.as_view(), name='profile_detail'),
-
+    path("", HomeView.as_view(), name="home"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("creatuser/", CreateUserView.as_view(), name="creat_user"),
+    path("creatprofile/", CreateProfileView.as_view(), name="creat_profile"),
+    path("editeuser/", EditeUserView.as_view(), name="edit_user"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("changepass/", ChangePasswordView.as_view(), name="change_pass"),
+    path("profiles/<int:pk>/", ProfileDetailView.as_view(), name="profile_detail"),
 ]
-
