b139718dccc83a0b10a4a6ef1fcb82083b15efd5
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-04 22:20:06 +0330

    Add  URLs to orders/urls.py

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+# from .views import AllOrders
+
+urlpatterns = [
+    path('', views.AllOrders.as_view(), name='all_orders'),
+    path('detail/<int:pk>', views.OrderDetail.as_view(), name='detail'),
+]
