488ec352bdfa1f65db8ba4a420daa85d60c0c03d
mohammadreza-n-fallah <mohammadrezafalah0987@gmail.com>
2024-03-05 18:31:20 +0330

    add urls for menu app

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('categories/',CategoryView.as_view(),name='categories' ),
     path('products/<int:id>',ProductView.as_view(),name='products' ),
