9f88a28cd9b43e14af7bd28f7967a0c7db9abeff
ashkan1592 <ashkan1592@gmail.com>
2024-03-05 18:03:45 +0330

    not paid order added

--- orders/managers.py
+++ orders/managers.py
@@ -9,3 +9,12 @@ class PaidOrderManager(models.Manager):
             return super().get_queryset().filter(is_paid=True)
         else:
             return super().get_queryset().none()
+
+
+class NotPaidOrderManager(models.Manager):
+    def get_queryset(self):
+        user_model = get_user_model()
+        if user_model.is_authenticated:
+            return super().get_queryset().filter(is_paid=False)
+        else:
+            return super().get_queryset().none()
